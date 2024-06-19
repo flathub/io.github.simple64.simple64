183b5dcecd3ededdff774965dfe6b66fd38bdd53
Logan McNaughton <848146+loganmc10@users.noreply.github.com>
2024-06-19 11:23:25 -0600

    Fix file open on KDE

--- gui-wrapper.sh
+++ gui-wrapper.sh
@@ -4,4 +4,11 @@ for i in {0..9}; do
     test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
 done
 
+# fix for https://github.com/simple64/simple64/issues/437
+chmod +w $XDG_CONFIG_HOME/kdeglobals
+cat > $XDG_CONFIG_HOME/kdeglobals << EOF
+[Development]
+AutoCheckAccelerators=false
+EOF
+
 exec /app/bin/simple64-gui "$@"
